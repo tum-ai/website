16372a42dd37ff443e81eea40291d39726342197
MunzerDw <munzerdwedari@gmail.com>
2023-06-06 00:28:59 +0200

    add header component

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -1,34 +1,11 @@
-import Image from 'next/image';
 import Button from '../../components/Button';
 import VerticalCards from '../../components/VerticalCards';
+import Header from './components/Header';
 
 export default function Home() {
 	return (
 		<>
-			<div className='py-16 px-8 bg-primary flex'>
-				<div className='m-auto lg:inline-block space-y-8'>
-					<Image
-						src={'/assets/logo_new_white_standard.png'}
-						alt='Logo'
-						width={450}
-						height={100}
-					/>
-					<div className='w-fit m-auto flex flex-col justify-center lg:space-x-4 lg:space-y-0 space-y-4 lg:flex-row'>
-						<Button
-							href={'https://www.tum-ai.com/forPartners.html'}
-							className='bg-primary-500'
-						>
-							Become a Partner
-						</Button>
-						<Button
-							href={'https://join-us.tum-ai.com/'}
-							className='bg-primary-500'
-						>
-							Become a Member
-						</Button>
-					</div>
-				</div>
-			</div>
+			<Header />
 			<div className='bg-primary-500 h-3'></div>
 			<VerticalCards
 				data={[
