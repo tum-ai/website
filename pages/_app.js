660f3acbb0c0b9c0dffc7319ade4c7a2f4de2eda
Simon Huang <simon.huang@tum.de>
2023-06-13 00:36:10 +0200

    Add prettier (#10)
    * Add prettier and format files
    
    * Add command for formatting and prettierignore

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import Footer from '../components/02_molecules/Footer';
-import Navbar from '../components/02_molecules/Navbar';
-import '../styles/globals.css';
+import Footer from "../components/02_molecules/Footer";
+import Navbar from "../components/02_molecules/Navbar";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-	return (
-		<>
-			<Navbar />
-			<Component {...pageProps} />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
