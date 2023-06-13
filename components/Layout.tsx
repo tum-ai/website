37e76c852a266d3c78381b365c9c4bac7e0bcf39
Simon Huang <simon.huang@tum.de>
2023-06-13 19:49:48 +0200

    Add inter font

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,13 @@
+import { inter } from "@styles/fonts";
 import Footer from "./02_molecules/Footer";
 import Navbar from "./02_molecules/Navbar";
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className={inter.className}>
       <Navbar />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 }
