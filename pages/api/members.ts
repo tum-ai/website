83775bfb465033d5e6330ecb6f89a7e3f7563e15
Simon Huang <simon.huang@tum.de>
2023-06-20 03:17:20 +0200

    Add notion database

--- pages/api/members.ts
+++ pages/api/members.ts
@@ -1,20 +1,25 @@
+import { Client } from "@notionhq/client";
+import { NotionMember } from "model/notionMember";
+
 export default async function handler(req, res) {
-  // TODO: This is mock data. Use notion db instead
-  const members = [
-    {
-      name: "David Podolskyi",
-      role: "President",
-      image: "https://www.tum-ai.com/assets/img/members/david_podolskyi.webp",
-      description:
-        "ML & Entrepreneurship enthusiast with focus on IoT and Recommender Systems",
-    },
-    {
-      name: "Hamze Al-Zamkan",
-      role: "President",
-      image: "https://www.tum-ai.com/assets/img/members/hamze_alzamkan.webp",
-      description: "lorem ipsum dolor sit amet",
-    },
-  ];
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+  const data = await notion.databases.query({
+    database_id: "6aba41de28f74ee881b698e959ce6ed3",
+  });
+
+  const notionMembers = data.results.map(
+    (user: any) => user?.properties as NotionMember
+  );
+
+  console.log(notionMembers);
+
+  const members = notionMembers
+    .map((member) => ({
+      name: member?.name?.title?.at(0)?.plain_text,
+    }))
+    .filter((member) => {
+      return typeof member.name === "string";
+    });
 
   res.status(200).json(members);
 }
