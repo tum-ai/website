7130d36e4014ff6b7cfb6f6a46405a6433485238
Simon Huang <simon.huang@tum.de>
2023-06-23 20:25:00 +0200

    Dev 162 implement member page (#13)
    * Add initial member content
    
    * Add members journey arrows
    
    * Add first draft of member page
    
    * Add member card description
    
    * Add api for getting members
    
    * Refactor members cards
    
    * add basic department card
    
    * Add department cards
    
    * Add only one card overlay visiable at a time
    
    * Add toggling of card overlay
    
    * Add fade animation for overlay
    
    * Remove member stats
    
    * Add notion database
    
    * Add roles to members
    
    * Move member model functions into api
    
    * Add members notion integration
    
    * Add truncating of members
    
    * Refactor member section into own component
    
    * Add todos
    
    * fix corner
    
    ---------
    
    Co-authored-by: MunzerDw <munzerdwedari@gmail.com>

---
+++ pages/api/members.ts
@@ -0,0 +1,36 @@
+import { Client } from "@notionhq/client";
+import { NotionMember } from "model/notionMember";
+
+export const isMember = (data: unknown): boolean => {
+  const member: any = data;
+  return (
+    typeof member.image === "string" &&
+    typeof member.description === "string" &&
+    typeof member.name === "string" &&
+    member.roles.every((role: any) => typeof role === "string") &&
+    member.departments.every((department: any) => typeof department === "string")
+  );
+};
+
+export default async function handler(req, res) {
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+  const data = await notion.databases.query({
+    database_id: "***REMOVED***",
+  });
+
+  const notionMembers = data.results as NotionMember[];
+
+  const members = notionMembers
+    .map((member: NotionMember) => ({
+      name: member?.properties?.Name?.title?.at(0)?.plain_text,
+      image: member?.icon?.file?.url,
+      roles: member?.properties?.Role.multi_select.map((role) => role.name),
+      departments: member?.properties?.[
+        "Functional or Mission-Based Department"
+      ].multi_select.map((department) => department.name),
+      description: "",
+    }))
+    .filter(isMember);
+
+  res.status(200).json(members);
+}
