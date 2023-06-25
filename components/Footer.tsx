5eddb5bd9cfcbc08a81f60fafc9d0dfb3e1a5340
Simon Huang <simon.huang@tum.de>
2023-06-25 19:53:41 +0200

    Add basic footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,27 +1,75 @@
 import Link from "next/link";
 import Image from "next/image";
+import {
+  faLinkedin,
+  faInstagram,
+  faSlack,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
   return (
     <footer className="bg-primary-950 text-white">
-      <div className="container mx-auto py-16 grid grid-cols-2">
+      <div className="container mx-auto grid gap-8 p-16 md:grid-cols-2">
         <div>
           <Image
             src={"/assets/logo_new_white_standard.png"}
+            className="mb-8 flex text-2xl"
             alt="Logo"
             width="128"
             height="32"
           />
-          <h3>TUM.ai &copy; 2022</h3>
-          <div>
-            <FontAw
+
+          <div className="space-x-4">
+            <Link href="https://www.instagram.com/tum.ai_official/">
+              <FontAwesomeIcon
+                icon={faInstagram}
+                size="2xl"
+                className="mb-4 text-white duration-500 hover:text-primary-300"
+              />
+            </Link>
+            <Link href="https://de.linkedin.com/company/tum-ai">
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                size="2xl"
+                className="mb-4 text-white duration-500 hover:text-primary-300"
+              />
+            </Link>
+            <Link href="https://join.slack.com/t/tumaipublic/shared_invite/zt-10kg0t1f9-JLRXDxY_d_vprKWgab0cVw">
+              <FontAwesomeIcon
+                icon={faSlack}
+                size="2xl"
+                className="mb-4 text-white duration-500 hover:text-primary-300"
+              />
+            </Link>
           </div>
         </div>
-        <div className="grid grid-cols-2">
-          <Link href="">Contact</Link>
-          <Link href="">Inprint</Link>
-          <Link href="">Terms &amp; Security</Link>
-          <Link href="">Request change</Link>
+
+        <div className="grid h-max sm:grid-cols-2 md:text-right">
+          <Link
+            className="py-2 underline duration-500 hover:text-primary-300"
+            href=""
+          >
+            Contact
+          </Link>
+          <Link
+            className="py-2 underline duration-500 hover:text-primary-300"
+            href="inprint"
+          >
+            Inprint
+          </Link>
+          <Link
+            className="py-2 underline duration-500 hover:text-primary-300"
+            href=""
+          >
+            Terms &amp; Security
+          </Link>
+          <Link
+            className="py-2 underline duration-500 hover:text-primary-300"
+            href="https://github.com/tum-ai/website-nextjs/"
+          >
+            Request change
+          </Link>
         </div>
       </div>
     </footer>
