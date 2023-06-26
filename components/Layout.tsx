aae67dd313771c1b8e2ec2a072d791c58ea8ff05
Simon Huang <simon.huang@tum.de>
2023-06-26 10:17:44 +0200

    Change inter font to IBM_plex_sans

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,10 @@
-import { inter } from "@styles/fonts";
+import { ibm_plex_sans } from "@styles/fonts";
 import Footer from "./02_molecules/Footer";
 import Navbar from "./02_molecules/Navbar";
 
 export default function Layout({ children }) {
   return (
-    <div className={inter.className}>
+    <div className={ibm_plex_sans.className}>
       <Navbar />
       <main>{children}</main>
       <Footer />
