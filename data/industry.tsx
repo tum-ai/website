4c7082d4b398fe116742832e1b038512c9203b13
Max-vS <117863553+Max-vS@users.noreply.github.com>
2023-06-28 23:24:38 +0200

    Dev 163 create industry page (#18)
    * DEV-163[Setup] - test new header component
    
    * DEV-163[feature] - adjust Header
    
    * DEV-163[feature] determine active tap proto
    
    * DEV-163[feature] For Students
    
    * DEV-163[feature] new card display proto
    
    * DEV-163[feature] Section component
    
    * DEV-163[feature] For Industry
    
    * dev-163[feature] cleanup and refactor
    
    * dev-163[fix] remove assets
    
    * dev-163[fix] style guide
    
    * dev-163[fix]
    
    * dev-163[feature] resizing picture
    
    * Format files
    
    * Remove login signup
    
    * Escape special chars
    
    * Fix link being off center on mobile
    
    * dev-163[fix] add correct img src
    
    * Remove heroicons react
    
    * Refactor tabs
    
    * Refactor to radix-ui
    
    * remove unused file
    
    * remove redundant assets
    
    * put stuff into components
    
    * address CI
    
    ---------
    
    Co-authored-by: Simon Huang <simon.huang@tum.de>
    Co-authored-by: MunzerDw <munzerdwedari@gmail.com>

---
+++ data/industry.tsx
@@ -0,0 +1,189 @@
+export const partners = [
+    {
+      href: "https://www.atoss.com/de",
+      src: "/assets/industry/partners/ATOSS.png",
+      alt: "atoss",
+    },
+    {
+      href: "https://www.hypovereinsbank.de",
+      src: "/assets/industry/partners/HVB_2.png",
+      alt: "hypovereinsbank",
+    },
+    {
+      href: "https://www.infineon.com/cms/de/",
+      src: "/assets/industry/partners/infineon_logo.png",
+      alt: "infineon",
+    },
+    {
+      href: "https://www.prosiebensat1.com",
+      src: "/assets/industry/partners/P7S1_transparent.png",
+      alt: "prosiebensat1",
+    },
+    {
+      href: "https://www.sportortho.mri.tum.de",
+      src: "/assets/industry/partners/MRI.png",
+      alt: "MRI",
+    },
+    {
+      href: "https://neuralprophet.com",
+      src: "/assets/industry/partners/neuralprophet_logo.png",
+      alt: "neuralprophet",
+    },
+    {
+      href: "https://eyeo.com",
+      src: "/assets/industry/partners/eyeo.png",
+      alt: "eyeo",
+    },
+    {
+      href: "https://gruppe.schwarz",
+      src: "/assets/industry/partners/schwarzgroup_edit_cropped.png",
+      alt: "Schwarz Gruppe",
+    },
+    {
+      href: "https://www.rohde-schwarz.com/de",
+      src: "/assets/industry/partners/RandS.png",
+      alt: "Rhode-Schwarz",
+    },
+  ];
+
+  export const projects = [
+    {
+      title: "Roland Berger - Cloud-based Data Processing",
+      image: "/assets/industry/project_cards/rolandberger_industry.png",
+      description: [
+        {
+          text: "Implementation of cloud-based web services, containing NLP Machine Learning models -",
+          link: [
+            {
+              url: "https://www.linkedin.com/in/stefanrmmr/",
+              displayText: "Stefan Rummer",
+            },
+            {
+              url: "https://www.linkedin.com/in/robin-mittas-a29a11201/",
+              displayText: "Robin Mittas",
+            },
+            {
+              url: "https://www.linkedin.com/in/vtq/",
+              displayText: "Tuan-Quang Vuong",
+            },
+          ],
+          moreText:
+            "built scalable APIs that were deployed to production globally. These enabled Roland Berger to automatically enrich their CRM systems with financial market insights and LinkedIn company data.",
+        },
+      ],
+      organization: "Roland Berger",
+      organizationLink: "https://www.rolandberger.com/de/",
+      time: "fall 2022",
+    },
+    {
+      title: "QuantCo - Virtual Biopsy",
+      image: "/assets/industry/project_cards/quantco.jpeg",
+      description: [
+        {
+          text: "4x stellar students supported QuantCo in their mission to revolutionize the way prostate cancer is detected leveraging ML-based virtual biopsy.",
+          link: [
+            {
+              url: "https://www.linkedin.com/in/richardgaus/",
+              displayText: "Richard Gaus",
+            },
+            {
+              url: "https://www.linkedin.com/in/cedrik-laue-770682203/",
+              displayText: "Cedrik Laue",
+            },
+          ],
+          moreText:
+            "Implemented algorithms for medical image processing, ranging from pre-processing, registration, all the way to the segmentation of MRI data.",
+        },
+      ],
+      organization: "QuantCo",
+      organizationLink: "https://quantco.com/",
+      time: "fall 2022",
+    },
+    {
+      title: "TUM MRI Radiology - Klinikum Rechts der Isar",
+      image: "/assets/industry/project_cards/radiologie.png",
+      description: [
+        {
+          text: "The Institute for Diagnostic and Interventional Radiology performs and evaluates examinations using ultrasound, conventional X-ray technology, CT and MRI. Project participants collaborated with radiologists to learn about the specifics of medical imaging formats (such as DICOM) and the basics of medical knowledge required for the task.",
+          // Update with links and moreText when more details available
+        },
+      ],
+      organization: "TUM MRI",
+      organizationLink: "https://www.mri.tum.de/", // Update if there's an actual link
+      time: "fall 2022", // Update if there's an actual date
+    },
+    {
+      title: "TURTLE - Maritime Matchmaking",
+      image: "/assets/industry/project_cards/seafarer.png",
+      description: [
+        {
+          text: "TURTLE empowers seafarers and enables a digital, efficient, and compliant market free from corruption and other illegal and immoral activities. We joined a team of industry professionals building a global online job marketplace that connects ship owners and seafarers directly, work in a fast-growing startup and with strong social impact!",
+        },
+      ],
+      organization: "TURTLE",
+      organizationLink:
+        "https://www.linkedin.com/company/turtle-gmbh/?originalSubdomain=de",
+      time: "spring 2022",
+    },
+    {
+      title: "Leevi Health - Baby Health Monitoring",
+      image: "/assets/industry/project_cards/leevi_baby.png",
+      description: [
+        {
+          text: "During this project we contributed to Leevi's mission of providing digital health solutions for infants. Leevi helps parents accurately understand the wellbeing of their babies through individual insights via a wearable bracelet that collects the babies vital and sleep parameters.",
+        },
+      ],
+      organization: "Leevi",
+      organizationLink: "https://leevi-health.com/",
+      time: "spring 2022",
+    },
+    {
+      title: "Cognote.ai - Medical Speech Recognition",
+      image: "/assets/industry/project_cards/prev_cognote.png",
+      description: [
+        {
+          text: "During this AI project, our team worked broadly on conversational speech recognition technology for the medical domain. This involved the assembly of a German speech dataset, training (and/or fine-tuning) large modern speech models on our compute infrastructure and evaluating their effectiveness relative to current cloud offerings.",
+        },
+      ],
+      organization: "Cognote",
+      organizationLink: "https://www.cognote.ai/",
+      time: "spring 2022",
+    },
+    {
+      title: "Presize.ai - Clothing size recommender systems",
+      image: "/assets/industry/project_cards/presize_resize.jpg",
+      description: [
+        {
+          text: "We created a recommender system for clothing sizes and benchmarked it against Presize’s own technology. This way we actively contributed of Presize's s vision of reducing the amount of paercel-returns.",
+        },
+      ],
+      organization: "presize.ai",
+      organizationLink: "https://www.presize.ai/",
+      time: "fall 2021",
+    },
+    {
+      title: "Heimkapital - Real estate price prediction",
+      image: "/assets/industry/project_cards/heimkapital_resized.jpg",
+      description: [
+        {
+          text: "We developed solutions to make an impact on the financial independence of homeowners by implementing an AI that can predict real estate prices based on population data.",
+        },
+      ],
+      organization: "Heimkapital",
+      organizationLink:
+        "https://www.heimkapital.de/?gclid=Cj0KCQjwqKuKBhCxARIsACf4XuFcI2JnKY0mJUc5_abF6uqJlJyi1Uqi291fM6qQD6V0WSy3aKzhFGMaArIQEALw_wcB",
+      time: "fall 2021",
+    },
+    {
+      title: "DynaGroup & Veritas PR - NLP paraphrasing",
+      image: "/assets/industry/project_cards/dyna_group_resize.jpg",
+      description: [
+        {
+          text: "We created an NLP-based system that can paraphrase sequences of text while reliably preserving the meaning - making online content creation easier and less-time consuming for smaller companies and non-profits.",
+        },
+      ],
+      organization: "DynaGroup",
+      organizationLink: "https://www.dynagroup.de/",
+      time: "fall 2021",
+    },
+  ];
