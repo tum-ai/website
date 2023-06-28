874ed59573ba75f2dfa2d5955462c6ee8b0443d8
Simon Huang <simon.huang@tum.de>
2023-06-28 21:44:17 +0200

    Change html element

--- components/Stat.tsx
+++ components/Stat.tsx
@@ -3,8 +3,8 @@ import { cx } from "class-variance-authority";
 export default function Stat2({ description, value, className = "text-white" }) {
   return (
     <div className={cx("flex h-40 w-48 max-w-sm flex-col items-center justify-center space-y-1 rounded-xl", className)}>
-      <dd className={`text-5xl font-semibold`}>{value}</dd>
-      <dt className={`text-md`}>{description}</dt>
+      <h3 className="text-5xl font-semibold">{value}</h3>
+      <p className="text-md">{description}</p>
     </div>
   );
 }
