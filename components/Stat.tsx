574d7f3ebdc468e668aab3f0bae89ba097ae2d7b
Max-vS <max@vonstorch.com>
2023-06-28 13:48:48 +0200

    dev-168[fix] dynamic classname

---
+++ components/Stat.tsx
@@ -0,0 +1,10 @@
+import cx from 'classnames';
+
+export default function Stat2({ description, value, className = "text-white" }) {
+  return (
+    <div className={cx("flex h-40 w-48 max-w-sm flex-col items-center justify-center space-y-1 rounded-xl", className)}>
+      <dd className={`text-5xl font-semibold`}>{value}</dd>
+      <dt className={`text-md`}>{description}</dt>
+    </div>
+  );
+}
