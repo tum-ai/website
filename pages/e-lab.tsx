fcad394305122ac2212a9d4b025f3e15d0e6a926
MunzerDw <munzerdwedari@gmail.com>
2023-06-29 20:35:10 +0200

    revert

--- pages/e-lab.tsx
+++ pages/e-lab.tsx
@@ -1,9 +1,17 @@
+import Logos from "@components/Logos";
 import { ELabHero } from "@components/heroes/Elab";
+import { partners } from "data/industry";
 
 export default function AIELab() {
   return (
     <>
       <ELabHero />
+      <section className="h-[200vh]">
+        <h1>Scrolltest</h1>
+      </section>
+      <Logos title="1. Premium Sponsors" data={partners} />
+      <Logos title="2. Sponsors" data={partners} />
+      <Logos title="3. Workshops and more by" data={partners} />
     </>
   );
 }
