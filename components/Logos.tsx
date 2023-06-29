27002459fd256bf750b8470bae4e305d61c433e0
MunzerDw <munzerdwedari@gmail.com>
2023-06-29 20:19:03 +0200

    linting

--- components/Logos.tsx
+++ components/Logos.tsx
@@ -3,41 +3,47 @@ import Link from "next/link";
 import Section from "./ui/Section";
 
 export interface Props {
-    title?: string,
-    data: {
-        "src": string,
-        "alt": string,
-        "href": string,
-    }[]
+  title?: string;
+  data: {
+    src: string;
+    alt: string;
+    href: string;
+  }[];
 }
 
 export default function Logos({ title, data }: Props) {
   return (
     <Section>
-        {title && <h2 className="text-2xl text-center font-bold flex items-center justify-center lg:space-x-6"><div className="h-1 w-32 border-t hidden lg:block border-black"></div> <span>{title}</span> <div className="h-1 w-32 border-t hidden lg:block border-black"></div></h2>}
-        <div className="flex flex-wrap items-center justify-center">
-            {data.map((object, index) => (
-            <div
-                className="px-4 py-2 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
-                key={object['src']}
-            >
-                <Link href={object.href} passHref>
-                <div
-                    className="min-w-160 flex h-full items-center justify-center p-8 grayscale 
+      {title && (
+        <h2 className="flex items-center justify-center text-center text-2xl font-bold lg:space-x-6">
+          <div className="hidden h-1 w-32 border-t border-black lg:block"></div>{" "}
+          <span>{title}</span>{" "}
+          <div className="hidden h-1 w-32 border-t border-black lg:block"></div>
+        </h2>
+      )}
+      <div className="flex flex-wrap items-center justify-center">
+        {data.map((object, index) => (
+          <div
+            className="px-4 py-2 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+            key={object["src"]}
+          >
+            <Link href={object.href} passHref>
+              <div
+                className="min-w-160 flex h-full items-center justify-center p-8 grayscale 
                         transition-all duration-200 ease-out hover:shadow-md hover:shadow-purple-800/5 hover:grayscale-0"
-                >
-                    <Image
-                    src={object.src}
-                    alt={object.alt || ""}
-                    width={160}
-                    height={50}
-                    objectFit="contain"
-                    />
-                </div>
-                </Link>
-            </div>
-            ))}
-        </div>
+              >
+                <Image
+                  src={object.src}
+                  alt={object.alt || ""}
+                  width={160}
+                  height={50}
+                  objectFit="contain"
+                />
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
     </Section>
   );
 }
--- data/e-lab.tsx
+++ data/e-lab.tsx
@@ -1,47 +1,47 @@
 export const partners = [
-    {
-      href: "https://www.atoss.com/de",
-      src: "/assets/industry/partners/ATOSS.png",
-      alt: "atoss",
-    },
-    {
-      href: "https://www.hypovereinsbank.de",
-      src: "/assets/industry/partners/HVB_2.png",
-      alt: "hypovereinsbank",
-    },
-    {
-      href: "https://www.infineon.com/cms/de/",
-      src: "/assets/industry/partners/infineon_logo.png",
-      alt: "infineon",
-    },
-    {
-      href: "https://www.prosiebensat1.com",
-      src: "/assets/industry/partners/P7S1_transparent.png",
-      alt: "prosiebensat1",
-    },
-    {
-      href: "https://www.sportortho.mri.tum.de",
-      src: "/assets/industry/partners/MRI.png",
-      alt: "MRI",
-    },
-    {
-      href: "https://neuralprophet.com",
-      src: "/assets/industry/partners/neuralprophet_logo.png",
-      alt: "neuralprophet",
-    },
-    {
-      href: "https://eyeo.com",
-      src: "/assets/industry/partners/eyeo.png",
-      alt: "eyeo",
-    },
-    {
-      href: "https://gruppe.schwarz",
-      src: "/assets/industry/partners/schwarzgroup_edit_cropped.png",
-      alt: "Schwarz Gruppe",
-    },
-    {
-      href: "https://www.rohde-schwarz.com/de",
-      src: "/assets/industry/partners/RandS.png",
-      alt: "Rhode-Schwarz",
-    },
-  ];
+  {
+    href: "https://www.atoss.com/de",
+    src: "/assets/industry/partners/ATOSS.png",
+    alt: "atoss",
+  },
+  {
+    href: "https://www.hypovereinsbank.de",
+    src: "/assets/industry/partners/HVB_2.png",
+    alt: "hypovereinsbank",
+  },
+  {
+    href: "https://www.infineon.com/cms/de/",
+    src: "/assets/industry/partners/infineon_logo.png",
+    alt: "infineon",
+  },
+  {
+    href: "https://www.prosiebensat1.com",
+    src: "/assets/industry/partners/P7S1_transparent.png",
+    alt: "prosiebensat1",
+  },
+  {
+    href: "https://www.sportortho.mri.tum.de",
+    src: "/assets/industry/partners/MRI.png",
+    alt: "MRI",
+  },
+  {
+    href: "https://neuralprophet.com",
+    src: "/assets/industry/partners/neuralprophet_logo.png",
+    alt: "neuralprophet",
+  },
+  {
+    href: "https://eyeo.com",
+    src: "/assets/industry/partners/eyeo.png",
+    alt: "eyeo",
+  },
+  {
+    href: "https://gruppe.schwarz",
+    src: "/assets/industry/partners/schwarzgroup_edit_cropped.png",
+    alt: "Schwarz Gruppe",
+  },
+  {
+    href: "https://www.rohde-schwarz.com/de",
+    src: "/assets/industry/partners/RandS.png",
+    alt: "Rhode-Schwarz",
+  },
+];
