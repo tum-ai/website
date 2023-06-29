9c5f431cf10c50d839eecb008197f0700df3c81a
Max-vS <117863553+Max-vS@users.noreply.github.com>
2023-06-29 20:37:05 +0200

    Dev 168 stat component (#20)
    * dev-168[feature] create 2 different stat comp
    
    * dev-168[feature] create StatSection
    
    * dev-168[fix] e-lab page
    
    * dev-168[fix] dynamic classname
    
    * dev-168[fix] clean up
    
    * Fix wrong import
    
    * Change html element
    
    * integrate stats section
    
    * clean up
    
    ---------
    
    Co-authored-by: Simon Huang <simon.huang@tum.de>

---
+++ components/Stat.tsx
@@ -0,0 +1,10 @@
+import { cx } from "class-variance-authority";
+
+export default function Stat2({ description, value, className = "text-white" }) {
+  return (
+    <div className={cx("flex h-40 w-48 max-w-sm flex-col items-center justify-center space-y-1 rounded-xl", className)}>
+      <h3 className="text-5xl font-bold">{value}</h3>
+      <p className="text-md">{description}</p>
+    </div>
+  );
+}
