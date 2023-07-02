7afa0d0d1cc39229927d2acf805305f886b8c89d
Simon Huang <simon.huang@tum.de>
2023-07-02 02:09:56 +0200

    Improve logo styles

--- components/Logos.tsx
+++ components/Logos.tsx
@@ -16,8 +16,7 @@ export default function Logos({ logos }: Props) {
       <div className="flex flex-wrap items-center justify-center">
         {logos.map((logo) => (
           <div
-            className="h-32 border px-4 py-2 grayscale transition-all duration-200 ease-out hover:shadow-md 
-            hover:shadow-purple-800/5 hover:grayscale-0 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+            className="h-32 w-full px-4 py-2 grayscale transition-all duration-200 ease-out hover:grayscale-0 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
             key={logo.src}
           >
             <Link href={logo.href} passHref>
