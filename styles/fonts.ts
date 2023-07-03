eb342d4e4c6696ebd5c033b7350ca7e1485e5bb6
Simon Huang <simon.huang@tum.de>
2023-07-03 19:41:48 +0200

    Fix text outline not working in e-lab hero

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,12 +1,16 @@
 import { Inter, Bitter } from "@next/font/google";
 
 const inter = Inter({
-  weight: "variable",
+  weight: "500",
   subsets: ["latin"],
 });
 const bitter = Bitter({
   weight: "variable",
   subsets: ["latin"],
 });
+const interBold = Inter({
+  weight: "700",
+  subsets: ["latin"],
+});
 
-export { inter, bitter };
+export { inter, interBold, bitter };
