7901cf7ecab1264e28aa0e3baf8f761e140f4a27
Max-vS <max@vonstorch.com>
2023-07-03 17:37:46 +0200

    remove import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
-import "../styles/utils/hide-scrollbar.css";
 
 export default function App({ Component, pageProps }) {
   const queryClient = new QueryClient();
