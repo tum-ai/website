1ca12b5bab14f8db118a2d66ed918ed07c6984e1
timonschramm <timonschramm@t-online.de>
2023-07-06 12:07:20 +0200

    used prettier, framer, next/image, removed dark/white theme/ made gray stronger for better opacity

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,40 +1,54 @@
 import Image from 'next/image';
+import { motion, useInView } from 'framer-motion';
 
 function Phase({ title, date, duration, imageSrc, phaseText }) {
+
+
     return (
-        <div className="relative flex justify-between">
+        <motion.div
+            className="relative flex justify-between"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }} // customize transition here
+            viewport={{ once: true }}
+
+        >
             <div className="flex flex-col items-center w-10 mr-4 md:w-24">
                 <div>
-                    <div className="flex items-center justify-center w-8 h-8 bg-blue-200 rounded-full dark:bg-gray-600">
-                        <div className="w-4 h-4 bg-blue-600 rounded-full dark:bg-blue-400"></div>
+                    <div className="flex items-center justify-center w-8 h-8 bg-blue-200 rounded-full ">
+                        <div className="w-4 h-4 bg-blue-600 rounded-full"></div>
                     </div>
                 </div>
-                <div className="w-px h-full bg-blue-300 dark:bg-gray-600"></div>
+                <div className="w-px h-full bg-blue-300"></div>
             </div>
             <div>
-                <h2 className="inline-block px-4 py-2 mb-4 text-xs font-medium text-gray-100 bg-gradient-to-r from-blue-500 to-blue-900 dark:from-blue-400 dark:to-blue-500 rounded-3xl dark:text-gray-100">
+                <h2 className="inline-block px-4 py-2 mb-4 text-xs font-medium text-gray-100 bg-gradient-to-r from-blue-500 to-blue-900 rounded-3xl">
                     {date} <br />
                     ({duration})
                 </h2>
-                <div className="relative flex-1 mb-10 bg-white border-b-4 border-blue-200 shadow dark:border-gray-700 rounded-3xl dark:bg-gray-900">
-                    <img
+                <div className="relative flex-1 mb-10 bg-white border-b-4 border-blue-200 shadow rounded-3xl">
+                    <Image
                         src={imageSrc}
                         alt={title}
-                        className="w-full mb-6"
+                        width={1920}
+                        height={1080}
                     />
+
                     <div className="relative z-20 p-6">
 
-                        <p className="mb-2 text-xl font-bold text-gray-600 dark:text-gray-400">
+                        <p className="mb-2 text-xl font-bold text-gray-900">
                             {title}
                         </p>
-                        <p className="text-gray-700 dark:text-gray-500">
+                        <p className="text-gray-800">
                             {phaseText}
                         </p>
 
                     </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
+
+
     );
 }
 
@@ -58,7 +72,7 @@ export default function Timeline() {
                     a team is admitted to the next stage.
                     "
                 />
-                    
+
                 <Phase
                     title="Idea Validation and Litmus Test"
                     date="16th October - 12th November 2023"
