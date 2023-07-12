340e2bceb3eac6143a496bc2c295c432771d73c5
MunzerDw <munzerdwedari@gmail.com>
2023-07-12 21:03:07 +0200

    sort members, filter members, sort roles

--- pages/api/members.ts
+++ pages/api/members.ts
@@ -31,6 +31,32 @@ export default async function handler(req, res) {
   const notion = new Client({ auth: process.env.NOTION_API_KEY });
   const data = await notion.databases.query({
     database_id: process.env.NOTION_MEMBERS_DATABASE_ID,
+    filter: {
+      "and": [
+        {
+          "property": "Name",
+          "title": {
+            "is_not_empty": true
+          }
+        },
+        {
+          "or": [
+            {
+              "property": "Role",
+              "multi_select": {
+                "contains": "President"
+              }
+            },
+            {
+              "property": "Role",
+              "multi_select": {
+                "contains": "Teamlead"
+              }
+            }
+          ]
+        }
+      ]
+    },
   });
 
   const notionMembers = data.results as NotionMember[];
@@ -39,14 +65,30 @@ export default async function handler(req, res) {
     .map((member: NotionMember) => ({
       name: member?.properties?.Name?.title?.at(0)?.plain_text,
       image: member?.icon?.file?.url,
-      roles: member?.properties?.Role.multi_select.map((role) => role.name),
+      roles: member?.properties?.Role.multi_select.map((role) => role.name).sort((a, b) => {
+        if (a == "President" && b !== "President") {
+          return -1
+        } else if (a == "Teamlead" && b !== "Teamlead") {
+          return -1
+        } else {
+          return 1
+        }
+      }),
       departments: member?.properties?.[
         "Functional or Mission-Based Department"
       ].multi_select.map((department) => department.name),
       description: "",
       degree: member?.properties?.Degree?.select?.name,
     }))
-    .sort((a, b) => (a.roles[0] < b.roles[0] ? 1 : -1))
+    .sort((a, b) => {
+      if (a.roles.includes("President") && !b.roles.includes("President")) {
+        return -1
+      } else if (a.roles.includes("Teamlead") && !b.roles.includes("Teamlead")) {
+        return -1
+      } else {
+        return 1
+      }
+    })
     .filter(isMember);
 
   res.status(200).json(members);
