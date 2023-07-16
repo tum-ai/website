15a99bec003592c7f4a4717702513b5176e0fd80
Efe Sener <73065502+EfeSenerr@users.noreply.github.com>
2023-07-17 01:46:44 +0200

    fix: added spacing between logos (#43)
    * fix: header
    
    * fix: quality of industy project timeline
    
    * feature: add logos of our partners and sponsors
    
    * fix: added spacing between logos
    
    * chore:format
    
    ---------
    
    Co-authored-by: Simon Huang <simon.huang@tum.de>

--- components/Logos.tsx
+++ components/Logos.tsx
@@ -13,7 +13,7 @@ export interface Props {
 export default function Logos({ logos }: Props) {
   return (
     <>
-      <div className="flex flex-wrap items-center justify-center">
+      <div className="flex flex-wrap items-center justify-center space-y-4">
         {logos.map((logo) => (
           <div
             className="h-32 w-full px-4 py-2 grayscale transition-all duration-200 ease-out hover:grayscale-0 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
