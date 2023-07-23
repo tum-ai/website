de0cd19c7cee443173dbc0aed9670c3f6c8f5cb3
MunzerDw <munzerdwedari@gmail.com>
2023-07-23 13:40:47 +0200

    remove old fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
-import { useEffect } from "react";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   const queryClient = new QueryClient();
-  useEffect(() => {
-    document.documentElement.style.setProperty(
-      "--vh",
-      window.innerHeight * 0.01 + "px"
-    );
-  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
