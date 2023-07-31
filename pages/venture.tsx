2761b3ae843e0051abb09403ce0fd235d0f32db7
MunzerDw <munzerdwedari@gmail.com>
2023-07-31 18:59:16 +0200

    prettier

--- pages/venture.tsx
+++ pages/venture.tsx
@@ -4,17 +4,12 @@ import { useEffect } from "react";
 
 export default function Venture() {
   useEffect(() => {
-    Router.push(
-      "/e-lab"
-    );
+    Router.push("/e-lab");
   }, []);
   return (
     <Section>
       <p>
-        Redirecting to{" "}
-        <a>
-          {`/e-lab`}
-        </a>
+        Redirecting to <a>{`/e-lab`}</a>
       </p>
     </Section>
   );
