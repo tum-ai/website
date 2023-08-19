22110b57a940d4665c6bb137976180163afb9d73
Max-vS <max@vonstorch.com>
2023-08-19 15:53:58 +0200

    change button color

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,15 +6,15 @@ import { Header } from "./Header";
 function Banner() {
   return (
     <div style={{ backgroundColor: "#EEEAFF" }} className="w-full py-5">
-      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8">
         <div>
           <span className="text-2xl">Upcoming AI Summit </span>
           <span className="text-md font-light">Oct 20th - 21st 2023</span>
         </div>
         <Link
-          style={{ backgroundColor: "#dfd6fe" }}
+          style={{ backgroundColor: "#925cf6" }}
           href="http://summit.tum-ai.com"
-          className="mx-2 rounded-lg px-5 py-2"
+          className="mx-2 rounded-lg px-5 py-2 text-center text-white shadow-sm"
         >
           Get Tickets
         </Link>
