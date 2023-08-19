941609f3144f9c5a19854ac6c28842a22d3f4d28
Max-vS <max@vonstorch.com>
2023-08-19 21:54:54 +0200

    small adjustements

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,22 +2,19 @@ import { inter } from "@styles/fonts";
 import Link from "next/link";
 import Footer from "./Footer";
 import { Header } from "./Header";
+import Button from "@components/ui/Button";
 
 function Banner() {
   return (
-    <div style={{ backgroundColor: "#EEEAFF" }} className="w-full py-3 sm:py-5">
+    <div className="w-full bg-purple-100/70 py-3 sm:py-5">
       <div className="mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col sm:flex-row sm:items-end sm:space-x-4">
-          <span className="text-xl">Upcoming AI Summit </span>
-          <span className="text-md font-light">Oct 20th - 21st 2023</span>
+          <h2 className="text-xl">Upcoming AI Summit </h2>
+          <p className="text-md font-light">Oct 20th - 21st 2023</p>
         </div>
-        <Link
-          style={{ backgroundColor: "#925cf6" }}
-          href="http://summit.tum-ai.com"
-          className="mx-2 rounded-lg px-2 py-2 text-center text-white shadow-sm sm:px-5"
-        >
-          Get Tickets
-        </Link>
+        <Button>
+          <Link href="http://summit.tum-ai.com">Get Tickets</Link>
+        </Button>
       </div>
     </div>
   );
