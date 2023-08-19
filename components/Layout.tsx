422ff57a1ee3b98f218d2ba7525b7b74f74fa6de
Max-vS <max@vonstorch.com>
2023-08-19 16:01:31 +0200

    adjust responsivness

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,16 +5,16 @@ import { Header } from "./Header";
 
 function Banner() {
   return (
-    <div style={{ backgroundColor: "#EEEAFF" }} className="w-full py-5">
+    <div style={{ backgroundColor: "#EEEAFF" }} className="w-full py-3 sm:py-5">
       <div className="mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8">
-        <div>
-          <span className="text-2xl">Upcoming AI Summit </span>
+        <div className="flex flex-col sm:flex-row sm:items-end sm:space-x-4">
+          <span className="text-xl">Upcoming AI Summit </span>
           <span className="text-md font-light">Oct 20th - 21st 2023</span>
         </div>
         <Link
           style={{ backgroundColor: "#925cf6" }}
           href="http://summit.tum-ai.com"
-          className="mx-2 rounded-lg px-5 py-2 text-center text-white shadow-sm"
+          className="mx-2 rounded-lg px-2 py-2 text-center text-white shadow-sm sm:px-5"
         >
           Get Tickets
         </Link>
