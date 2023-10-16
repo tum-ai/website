eb92efdb3554cba3f9aabc985a014d4fd01aea9d
timonschramm <timonschramm@t-online.de>
2023-10-16 22:39:20 +0200

    -feat: added a width field to the Logos component, only effecting venture

--- data/partners.tsx
+++ data/partners.tsx
@@ -3,126 +3,175 @@ export const partners_collabrated_with = [
     href: "https://www.microsoft.com/de-de/about",
     src: "/assets/partners_sponsors/Microsoft_Logo.png",
     alt: "Microsoft",
+    width: 160,
   },
   {
     href: "https://www.ibm.com/de-de",
     src: "/assets/partners_sponsors/ibm_logo.png",
     alt: "IBM",
+    width: 160,
+
   },
   {
     href: "https://www.appliedai.de/de/",
     src: "/assets/partners_sponsors/appliedai_logo.png",
     alt: "Applied AI",
+    width: 160,
+
   },
   {
     href: "https://www.unternehmertum.de/",
     src: "/assets/partners_sponsors/UnternehmerTUM.webp",
     alt: "UnternehmerTUM",
+    width: 160,
+
   },
   {
     href: "https://www2.deloitte.com/de/de/pages/risk/solutions/aistudio.html",
     src: "/assets/partners_sponsors/deloitte.png",
     alt: "Deloitte",
+    width: 160,
+
   },
   {
     href: "https://www.bcg.com/beyond-consulting/bcg-gamma/overview",
     src: "/assets/partners_sponsors/bcggamma_logo.png",
     alt: "BCG Gamma",
+    width: 160,
+
   },
   {
     href: "https://about.google/",
     src: "/assets/partners_sponsors/google_logo.png",
     alt: "Google",
+    width: 160,
+
   },
   {
     href: "https://openai.com/",
     src: "/assets/partners_sponsors/openai_logo.png",
     alt: "OpenAI",
+    width: 160,
+
   },
   {
     href: "https://www.netapp.com/de/",
     src: "/assets/partners_sponsors/NetApp_logo.png",
     alt: "NetApp",
+    width: 160,
+
   },
   {
     href: "https://www.siemens.com/de/de.html",
     src: "/assets/partners_sponsors/siemens-logo-default.svg",
     alt: "Siemens",
+    width: 160,
+
   },
   {
     href: "https://www.ryver.ai/",
     src: "/assets/partners_sponsors/ryverai.png",
     alt: "Ryver AI",
+    width: 160,
+
   },
   {
     href: "https://www.10xfounders.com/",
     src: "/assets/partners_sponsors/10xfounderslogo.png",
     alt: "10x Founders",
+    width: 160,
+
   },
   {
     href: "https://www.tngtech.com/index.html",
     src: "/assets/partners_sponsors/TNG_logo.png",
     alt: "TNG Tech",
+    width: 160,
+
   },
   {
     href: "https://www.infineon.com/",
     src: "/assets/partners_sponsors/infineon_logo.png",
     alt: "Infineon",
+    width: 160,
+
   },
   {
     href: "https://www.rolandberger.com/de/",
     src: "/assets/partners_sponsors/rolandberger_logo.png",
     alt: "Roland Berger",
+    width: 160,
+
   },
   {
     href: "https://quantco.com/",
     src: "/assets/partners_sponsors/quantco_logo.png",
     alt: "QuantCo",
+    width: 160,
+
   },
   {
     href: "https://www.aleph-alpha.com/",
     src: "/assets/partners_sponsors/alephalpha.png",
     alt: "Aleph Alpha",
+    width: 160,
+
   },
   {
     href: "https://www.cherry.vc/",
     src: "/assets/partners_sponsors/cherryVC.png",
     alt: "Cherry VC",
+    width: 160,
+
   },
   {
     href: "https://www.speedinvest.com/",
     src: "/assets/partners_sponsors/speedinvest.png",
     alt: "Speedinvest",
+    width: 160,
+
   },
   {
     href: "https://earlybird.com/",
     src: "/assets/partners_sponsors/earlybirdvc.png",
     alt: "Earlybird VC",
+    width: 160,
+
   },
   {
     href: "https://www.etventure.de/ey/",
     src: "/assets/partners_sponsors/EYetventure.png",
     alt: "EY Etventure",
+    width: 160,
+
   },
   {
     href: "https://jina.ai/",
     src: "/assets/partners_sponsors/jina_ico.png",
     alt: "Jina AI",
+    width: 160,
+
   },
   {
     href: "https://wandb.ai/site",
     src: "/assets/partners_sponsors/wandb_logo.png",
     alt: "Weights and Biases",
+    width: 160,
+
   },
   {
     href: "https://www.mri.tum.de/",
     src: "/assets/partners_sponsors/Klinikum_rechts_der_Isar_logo.svg",
     alt: "Klinikum Rechts der Isar",
+    width: 160,
+
   },
   {
     href: "https://www.avimedical.com/",
     src: "/assets/partners_sponsors/avi_medical_logo.png",
     alt: "Avi Medical",
+    width: 160,
+
   },
 ];
 
@@ -131,50 +180,70 @@ export const initiatives_collabrated_with = [
     href: "https://www.cdtm.de/",
     src: "/assets/partners_sponsors/cdtm_logo.png",
     alt: "CDTM",
+    width: 160,
+
   },
   {
     href: "https://www.linkedin.com/company/entreprenow-community/",
     src: "/assets/partners_sponsors/entreprenow.jpg",
     alt: "EntrepreNow Community",
+    width: 160,
+
   },
   {
     href: "https://www.startmunich.de/",
     src: "/assets/partners_sponsors/start_munich.png",
     alt: "Start Munich",
+    width: 160,
+
   },
   {
     href: "https://www.linkedin.com/company/knust-coe-ic/about/",
     src: "/assets/partners_sponsors/knust_innovationcentre.jpg",
     alt: "Knust CoE IC",
+    width: 160,
+
   },
   {
     href: "https://gdsc.community.dev/technical-university-of-munich/",
     src: "/assets/partners_sponsors/gdsc.png",
     alt: "GDSC",
+    width: 160,
+
   },
   {
     href: "https://analytics-club.org/wordpress/",
     src: "/assets/partners_sponsors/eth_ac.jpg",
     alt: "ETH Analytics Club",
+    width: 160,
+
   },
   {
     href: "https://q-summit.com",
     src: "/assets/partners_sponsors/qsummit.png",
     alt: "QSummit",
+    width: 160,
+
   },
   {
     href: "https://enactus-muenchen.de/",
     src: "/assets/partners_sponsors/enactus.jpg",
     alt: "Enactus Munich",
+    width: 160,
+
   },
   {
     href: "https://www.linkedin.com/company/initiatives-for-humanity/",
     src: "/assets/partners_sponsors/initiatives_fh.jpg",
     alt: "Initiatives for Humanity",
+    width: 160,
+
   },
   {
     href: "https://www.mcml.ai",
     src: "/assets/partners_sponsors/MCML_Logo_2.png",
     alt: "MCML",
+    width: 160,
+
   },
 ];
