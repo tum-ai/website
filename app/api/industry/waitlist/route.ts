8f1436c34813c396048b922ae6ddd80c542156d4
simonhng <simon.huang@tum.de>
2024-01-29 18:25:47 +0100

    refactor waitlist to app routing

--- app/api/industry/waitlist/route.ts
+++ app/api/industry/waitlist/route.ts
@@ -1,13 +1,10 @@
 import { kv } from "@vercel/kv";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const email = req.body.data.email;
+export default async function POST(request: NextRequest) {
+  const email = await request.json().then((res) => res.email);
 
   await kv.sadd("ip6_waitlist", email);
 
-  return res.status(200).json(email);
+  return NextResponse.json(email);
 }
