7ed2f4a891e79ffa7890b45b088dd3efc67b892c
simonhng <simon.huang@tum.de>
2024-03-02 05:34:15 +0800

    fix build issues

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
-export const Providers = ({ children }) => {
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export const Providers = ({ children }: ProvidersProps) => {
   const [queryClient] = React.useState(() => new QueryClient());
 
   return (
